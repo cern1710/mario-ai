f40a195f3da0f936e0a672e416385c3e2b0d0ac6
Samuel Wu <cernunnos1710@gmail.com>
2024-02-07 23:09:04 +0000

    added some variables to Agent and create an environment.py file for wrappers

--- src/agent.py
+++ src/agent.py
@@ -2,8 +2,41 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from nes_py.wrappers import JoypadSpace
 from torch import Tensor
 
 class Agent:
-    def __init__(self):
-        pass
+    """TODO: finish this
+
+    Attributes:
+        env (JoypadSpace):
+        discount (float):
+        learning_rate (float):
+        entropy_coeff (float):
+        epsilon (float):
+        tau (float):
+        num_epochs (int):
+        batch_size (int):
+        save_dir (str):
+    """
+    def __init__(
+            self,
+            env: JoypadSpace,
+            discount: float,
+            learning_rate: float,
+            entropy_coeff: float,
+            epsilon: float,
+            tau: float,
+            num_epochs: int,
+            batch_size: int,
+            save_dir: str,
+        ) -> None:
+        self.env = env
+        self.discount = discount
+        self.learning_rate = learning_rate
+        self.entropy_coeff = entropy_coeff
+        self.epsilon = epsilon
+        self.tau = tau
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.save_dir = save_dir
---
+++ src/environment.py
@@ -0,0 +1,7 @@
+import gym_super_mario_bros
+
+class Reward(gym.Wrapper):
+    """TODO: comments
+    """
+    def __init__(self):
+        pass
