393722706fa6e7bed469fb0d9490f61f1adb2eae
Samuel Wu <cernunnos1710@gmail.com>
2024-02-07 22:24:51 +0000

    could run gym super mario bros locally; need to start training RL soon

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import torch
+import numpy as np
+import pygame
+import gym_super_mario_bros
 import sys
 sys.path.append('./src')
 
 from model import Model
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
 # Source:
 # https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
@@ -33,7 +38,49 @@ def test_model(model: Model):
     print(bcolors.OKGREEN + "PASSED Test 1: Model's output shapes are correct." + bcolors.OKGREEN)
 
 if __name__ == "__main__":
+    np.bool8 = np.bool_ # Avoid warnings
     num_inputs = 3
     num_actions = 10
     model = Model(num_inputs, num_actions)
     test_model(model)
+
+    pygame.init()
+    env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0',
+            apply_api_compatibility=True, render_mode="human")
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+    key_action_mapping = {
+        pygame.K_RIGHT: 1,  # Walk right
+        pygame.K_SPACE: 2,  # Jump + right
+        pygame.K_DOWN: 3,   # Run + right
+        pygame.K_a: 4,      # Run / shoot fireballs
+        pygame.K_UP: 5,     # Jump up directly
+        pygame.K_LEFT: 6,   # Walk left
+    }
+
+    screen = pygame.display.set_mode((84, 84))  # NES resolution
+    clock = pygame.time.Clock()
+
+    done = False
+    env.reset()
+    last_action = 0
+
+    while not done:
+        env.render()
+        action = last_action
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key in key_action_mapping:
+                    action = key_action_mapping[event.key]
+                    last_action = action
+            elif event.type == pygame.QUIT:
+                done = True
+            else:
+                last_action = 0
+
+        obs, reward, terminated, truncated, info = env.step(action)
+        clock.tick(60)
+
+    pygame.quit()
+    env.close()
